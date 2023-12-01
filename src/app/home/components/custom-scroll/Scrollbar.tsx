110d27109d956506fec8f47276cc316d1df0375a
Matheus Paice <matheus.paice@jbtec.com.br>
2023-12-01 19:04:38 -0300

    fix: remove navigator for success build

--- src/app/home/components/custom-scroll/Scrollbar.tsx
+++ src/app/home/components/custom-scroll/Scrollbar.tsx
@@ -16,13 +16,13 @@ interface PropsType {
 const Scrollbar = (props: PropsType) => {
   const { children, sx, ...other } = props;
 
-  const isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
-  if (isMobile) {
-    return <Box sx={{ overflowX: "auto" }}>{children}</Box>;
-  }
+  // const isMobile =
+  //   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  //     navigator.userAgent
+  //   );
+  // if (isMobile) {
+  //   return <Box sx={{ overflowX: "auto" }}>{children}</Box>;
+  // }
 
   return (
     <SimpleBarStyle sx={sx} {...other}>
