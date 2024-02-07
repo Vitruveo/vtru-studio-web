0d6e786f34310ba9e2169802b4630f12bff822ec
marcos <marcos.barboza@jbtec.com.br>
2024-02-07 18:02:11 -0300

    fix: next domains

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
             'vitruveo-studio-qa-assets.s3.amazonaws.com',
             'vitruveo-studio-dev-assets.s3.amazonaws.com',
             'vitruveo-studio-qa-general.s3.amazonaws.com',
+            'vitruveo-studio-production-assets.s3.amazonaws.com',
+            'vitruveo-studio-production-general.s3.amazonaws.com',
         ],
     },
     reactStrictMode: false,
