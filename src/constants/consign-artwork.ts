eb9cfa179270dc56a1651cce6ca5b994f7a2795c
Matheus Paice <matheus.paice@jbtec.com.br>
2024-04-05 13:14:05 -0300

    fix: set default URL store dev

--- src/constants/consign-artwork.ts
+++ src/constants/consign-artwork.ts
@@ -1,1 +1,2 @@
-export const CONSIGN_ARTWORK_PREVIEW_URL = process.env.NEXT_PUBLIC_CONSIGN_ARTWORK_PREVIEW_URL ?? 'http://';
+export const CONSIGN_ARTWORK_PREVIEW_URL =
+    process.env.NEXT_PUBLIC_CONSIGN_ARTWORK_PREVIEW_URL ?? 'https://store.vtru.dev';
