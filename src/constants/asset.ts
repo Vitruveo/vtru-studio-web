b3d9aa7ae7fcbad9fcbbfa48326b0923c65fa01d
Matheus Paice <matheus.paice@jbtec.com.br>
2024-04-17 17:37:10 -0300

    fix: correct default asset storage name

--- src/constants/asset.ts
+++ src/constants/asset.ts
@@ -4,4 +4,4 @@ export const ASSET_STORAGE_URL =
 export const GENERAL_STORAGE_URL =
     process.env.NEXT_PUBLIC_GENERAL_STORAGE_URL || 'https://vitruveo-studio-dev-general.s3.amazonaws.com';
 
-export const ASSET_STORAGE_BUCKET = process.env.NEXT_PUBLIC_ASSET_STORAGE_BUCKET || 'vitruveo-studio-dev-assets';
+export const ASSET_STORAGE_BUCKET = process.env.NEXT_PUBLIC_ASSET_STORAGE_BUCKET || 'vitruveo-studio-qa-assets';
