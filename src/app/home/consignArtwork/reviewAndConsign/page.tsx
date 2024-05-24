4008a771342a54b4612ff6ef64ac1584e24bfcce
Matheus Paice <matheus.paice@jbtec.com.br>
2024-05-24 12:33:25 -0300

    fix: remove validation wallet on review

--- src/app/home/consignArtwork/reviewAndConsign/page.tsx
+++ src/app/home/consignArtwork/reviewAndConsign/page.tsx
@@ -82,27 +82,27 @@ const ConsignArtwork = () => {
     ];
 
     /* HANDLE WALLET CONNECTION */
-    useEffect(() => {
-        (async () => {
-            let hasWallet = false;
-            if (isConnected && address) {
-                for (const wallet of userWallets) {
-                    if (wallet.address === address) {
-                        dispatch(consignArtworkActionsCreators.setPreviewAndConsignWallet(wallet.address));
-                        hasWallet = true;
-                        break;
-                    }
-                }
-                if (!hasWallet) {
-                    toastr.display({
-                        type: 'error',
-                        message: 'Wallet not found, please add it to your account at your profile.',
-                    });
-                    await disconnectWallet();
-                }
-            }
-        })();
-    }, [isConnected, address]);
+    // useEffect(() => {
+    //     (async () => {
+    //         let hasWallet = false;
+    //         if (isConnected && address) {
+    //             for (const wallet of userWallets) {
+    //                 if (wallet.address === address) {
+    //                     dispatch(consignArtworkActionsCreators.setPreviewAndConsignWallet(wallet.address));
+    //                     hasWallet = true;
+    //                     break;
+    //                 }
+    //             }
+    //             if (!hasWallet) {
+    //                 toastr.display({
+    //                     type: 'error',
+    //                     message: 'Wallet not found, please add it to your account at your profile.',
+    //                 });
+    //                 await disconnectWallet();
+    //             }
+    //         }
+    //     })();
+    // }, [isConnected, address]);
 
     useEffect(() => {
         dispatch(validationConsignThunk());
