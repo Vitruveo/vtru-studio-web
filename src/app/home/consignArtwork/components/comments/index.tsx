4b29213d8ea6a4204f2f9e5ffabd66a0af0f8f85
Luan Almeida <luan.almeida@jbtec.com.br>
2024-08-09 22:46:43 -0300

    style: improve css to comment list

--- src/app/home/consignArtwork/components/comments/index.tsx
+++ src/app/home/consignArtwork/components/comments/index.tsx
@@ -1,5 +1,6 @@
 import { getRequestConsignCommentsThunk } from '@/features/asset/thunks';
 import { useDispatch, useSelector } from '@/store/hooks';
+import { formatDate } from '@/utils/formatDate';
 import { Box, Typography } from '@mui/material';
 import { useEffect } from 'react';
 
@@ -17,14 +18,29 @@ export default function Comments({ assetId }: CommentsProps) {
     if (comments?.length === 0) return;
 
     return (
-        <Box>
-            <Typography variant="h6">Moderator&apos;s Note</Typography>
-            <Typography variant="body1">{assetId}</Typography>
+        <Box width={'40vw'}>
+            <Typography variant="h5" mb={1}>
+                Moderator&apos;s Note
+            </Typography>
             <Box>
                 {comments?.map((comment) => (
-                    <Box key={comment.id}>
-                        <Typography variant="body1">{comment.comment}</Typography>
-                        <Typography variant="caption">{comment.when}</Typography>
+                    <Box
+                        key={comment.id}
+                        display="flex"
+                        flexDirection="column"
+                        p={2}
+                        border="2px solid #47D7BC"
+                        borderRadius={2}
+                        mb={1}
+                    >
+                        <Box display="flex" justifyContent="space-between">
+                            <Typography variant="body1" flexGrow={1} mr={2}>
+                                {comment.comment}
+                            </Typography>
+                            <Typography variant="caption" flexShrink={0}>
+                                {formatDate(comment.when)}
+                            </Typography>
+                        </Box>
                     </Box>
                 ))}
             </Box>
