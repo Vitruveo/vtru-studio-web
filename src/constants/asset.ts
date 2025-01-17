714b2dca1ef6ade03d6c914e62260ef31ab77f6a
Matheus Paice <paice.zinner@gmail.com>
2025-01-17 11:36:24 -0300

    fix: correct env stores

--- src/constants/asset.ts
+++ src/constants/asset.ts
@@ -5,7 +5,7 @@ export const GENERAL_STORAGE_URL =
     process.env.NEXT_PUBLIC_GENERAL_STORAGE_URL || 'https://vitruveo-studio-dev-general.s3.amazonaws.com';
 
 export const STORE_STORAGE_URL =
-    process.env.NEXT_PUBLIC_STORE_STORAGE_URL || 'https://vitruveo-studio-dev-stores.s3.amazonaws.com';
+    process.env.NEXT_PUBLIC_STORES_STORAGE_URL || 'https://vitruveo-studio-dev-stores.s3.amazonaws.com';
 
 export const ASSET_STORAGE_BUCKET = process.env.NEXT_PUBLIC_ASSET_STORAGE_BUCKET || 'vitruveo-studio-qa-assets';
 
