3f28bc573c989500138f7f8fe4e34f0f8788c960
Luan Almeida <luan.almeida@jbtec.com.br>
2025-02-19 10:35:46 -0300

    feat: enable stores in project menu

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,5 @@
+export const NODE_ENV = process.env.NEXT_PUBLIC_NODE_ENV || 'qa';
+
 export const BASE_URL_API = process.env.NEXT_PUBLIC_BASE_URL || 'http://127.0.0.1:5001';
 export const BASE_URL_BATCH = process.env.NEXT_PUBLIC_BASE_URL_BATCH || 'http://localhost:4000';
 export const BASE_URL_API3 = process.env.NEXT_PUBLIC_BASE_URL_API3 || 'http://localhost:5003';
