971b5d62724700c208ece9703387abad380cd126
Luan Almeida <luan.almeida@jbtec.com.br>
2025-03-19 09:17:33 -0300

    feat: gradient in sidebar

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -250,11 +250,7 @@ export default function Home() {
                 <Box
                     pr={1}
                     sx={{
-                        maxHeight: lgUp
-                            ? 'calc(100vh - 100px)'
-                            : mdUp || smUp
-                              ? 'calc(100vh - 400px)'
-                              : 'calc(100vh - 500px)',
+                        maxHeight: 'calc(100vh - 100px)',
                         overflowY: 'scroll',
                         overflowX: 'hidden',
                     }}
