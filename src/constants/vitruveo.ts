5a3c8840b6c873d33a6216d8d19790fa9cfedd7f
Luan Almeida <luan.almeida@jbtec.com.br>
2025-10-08 13:40:40 -0300

    hotifx: put vitruveo urls as default

--- src/constants/vitruveo.ts
+++ src/constants/vitruveo.ts
@@ -2,6 +2,6 @@ export const VITRUVEO_URL = process.env.NEXT_PUBLIC_VITRUVEO_URL || 'https://vit
 export const DREAMVERSE_URL = process.env.NEXT_PUBLIC_DREAMVERSE_URL || 'https://dreamverse.vitruveo.xyz';
 export const VTRU_SCOPE_URL = process.env.NEXT_PUBLIC_VTRU_SCOPE_URL || 'https://scope.vitruveo.xyz';
 export const REDIRECTS_JSON =
-    process.env.NEXT_PUBLIC_REDIRECTS_JSON || 'https://luanbian.github.io/redirects-vtru/data.json';
+    process.env.NEXT_PUBLIC_REDIRECTS_JSON || 'https://vitruveo.github.io/vtru-settings/redirects.json';
 export const SYSTEM_STATUS_JSON =
-    process.env.NEXT_PUBLIC_SYSTEM_STATUS_JSON || 'https://luanbian.github.io/system-status-vtru/data.json';
+    process.env.NEXT_PUBLIC_SYSTEM_STATUS_JSON || 'https://vitruveo.github.io/vtru-settings/system-status.json';
